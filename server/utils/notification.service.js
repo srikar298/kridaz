549ba83c16408cbd194076d15e2e58f3bf6b690f
Prince364133 <monsterprince3633@gmail.com>
2026-06-28 17:18:09 +0530

    fix: set correct button_1 object properties (type, sub_type, index, value) for MSG91 WhatsApp template

--- server/utils/notification.service.js
+++ server/utils/notification.service.js
@@ -54,7 +54,9 @@ export const sendWhatsAppMessage = async (
       const otpTemplate = process.env.MSG91_WHATSAPP_OTP_TEMPLATE || "otp_verification";
       if (templateName === otpTemplate && params.length > 0) {
         componentsObj["button_1"] = {
-          type: "text",
+          type: "button",
+          sub_type: "url",
+          index: "0",
           value: String(params[0]),
         };
       }
